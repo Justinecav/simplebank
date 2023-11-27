ca96d1086afd36bac8c82d0a552e4c2559c3c8ab
jestine <jestinerd10@outlook.com>
2023-11-27 19:57:22 +0530

    JWT API

--- api/server.go
+++ api/server.go
@@ -1,7 +1,11 @@
 package api
 
 import (
+	"fmt"
+
 	db "github.com/Justinecav/simplebank.git/db/sqlc"
+	"github.com/Justinecav/simplebank.git/token"
+	"github.com/Justinecav/simplebank.git/util"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
@@ -9,30 +13,33 @@ import (
 
 //Server serves http requests for banking service
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
 //NewServer creates a new http server and set up routing
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
-	router := gin.Default()
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token: %w", err)
+	}
+
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		//cuurency repaces "one of " with "currency"
 		v.RegisterValidation("currency", validCurrency)
 	}
 
-	router.POST("/accounts", server.createAccount)
-	router.GET("/accounts/:id", server.getAccount)
-	router.GET("/accounts", server.listAccounts)
-
-	router.POST("/transfers", server.createTransfer)
-
-	router.POST("/users", server.createUser)
-
-	server.router = router
-	return server
+	server.setupRouter()
+	return server, nil
 }
 
 //Start runs the http server at a specific address
@@ -42,4 +49,17 @@ func (sever *Server) Start(address string) error {
 
 func errorResponse(err error) gin.H {
 	return gin.H{"error": err.Error()}
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
+
+	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.POST("/transfers", server.createTransfer)
+
+	server.router = router
 }
--- api/user_test.go
+++ api/user_test.go
@@ -78,7 +78,9 @@ func TestCreateUserAPI(t *testing.T) {
 			},
 			checkResponse: func(recorder *httptest.ResponseRecorder) {
 				require.Equal(t, http.StatusOK, recorder.Code)
-				requireBodyMatchUser(t, recorder.Body, user)
+				actual := recorder.Body
+				expected := user
+				requireBodyMatchUser(t, actual, expected)
 			},
 		},
 		{
@@ -180,7 +182,7 @@ func TestCreateUserAPI(t *testing.T) {
 			store := mockdb.NewMockStore(ctrl)
 			tc.buildStubs(store)
 
-			server := NewServer(store)
+			server := newTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			// Marshal body data to JSON
