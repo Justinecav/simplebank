af895cf598b362a7a2089de58878ec61004bfc28
jestine <jestinerd10@outlook.com>
2023-11-27 19:57:22 +0530

    JWT API

--- api/main_test.go
+++ api/main_test.go
@@ -3,9 +3,27 @@ package api
 import (
 	"os"
 	"testing"
+	"time"
 
+	db "github.com/Justinecav/simplebank.git/db/sqlc"
+	"github.com/Justinecav/simplebank.git/util"
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
 )
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, store)
+
+	require.NoError(t, err)
+
+	return server
+
+}
 
 //this function is used to see more clear logs
 func TestMain(m *testing.M) {
